b5d1955858f1aa5c2b47f3c4cdc57825f933e5a4
gazoil2 <benicio920179@gmail.com>
2025-07-01 00:49:14 -0300

    new structure monolitic

--- apps/backend/prisma/migrations/20250701021140_add_cart_status_and_price/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateEnum
-CREATE TYPE "CartStatus" AS ENUM ('PENDING', 'COMPLETED', 'CANCELLED');
-
--- AlterTable
-ALTER TABLE "Cart" ADD COLUMN     "status" "CartStatus" NOT NULL DEFAULT 'PENDING';
-
--- AlterTable
-ALTER TABLE "CartItem" ADD COLUMN     "price" DOUBLE PRECISION NOT NULL DEFAULT 0;
